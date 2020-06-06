415d8c55a6f409dc81cae2e5af25805e62a30fae
Jason Cox <jasoncox@mac.com>
2020-06-05 22:27:59 -0400

    Initial Commit

---
+++ Project4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project4
+//
+//  Created by Jason Cox on 6/5/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Jason Cox on 6/5/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
