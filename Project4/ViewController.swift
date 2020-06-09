1b8a1f6472bc9dfcfa0343b435c4bcf6b93cc731
Jason Cox <jasoncox@mac.com>
2020-06-09 14:54:47 -0400

    Added blue outline to show which view is selected (focused)

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -9,9 +9,11 @@
 import Cocoa
 import WebKit
 
-class ViewController: NSViewController, WKNavigationDelegate {
+class ViewController: NSViewController, WKNavigationDelegate, NSGestureRecognizerDelegate {
 
     var rows: NSStackView!
+    var selectedWebView: WKWebView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,10 +55,66 @@ class ViewController: NSViewController, WKNavigationDelegate {
     }
 
     @IBAction func adjustRows(_ sender: NSSegmentedControl) {
+        if sender.selectedSegment == 0 {
+            // we're adding a new row
+            // count how many columns we have so far
+            let columnCount = (rows.arrangedSubviews[0] as! NSStackView).arrangedSubviews.count
+            
+            // make a new array of web views that contain the correct number of columns
+            let viewArray = (0 ..< columnCount).map {_ in makeWebView() }
+            
+            // use that web view array to create a new stack view
+            let row = NSStackView(views: viewArray)
+            
+            // make the stack view size its children equally, then add it to our 'rows; array
+            row.distribution = .fillEqually
+            rows.addArrangedSubview(row)
+        } else {
+            // we're deleting a row
+            
+            // make sure we have at least 2 rows
+            guard rows.arrangedSubviews.count > 1 else { return }
+            
+            // pull out the final row, and make sure it's a stack view
+            guard let rowToRemove = rows.arrangedSubviews.last as? NSStackView else { return }
+            
+            // loop through each web vview in the row, removing it from the screen
+            for cell in rowToRemove.arrangedSubviews {
+                cell.removeFromSuperview()
+            }
+            
+            // finally, remove the whole stack view row
+            rows.removeArrangedSubview(rowToRemove)
+        }
         
     }
     
     @IBAction func adjustColumns(_ sender: NSSegmentedControl) {
+        
+        if sender.selectedSegment == 0 {
+            // we need to add a column
+            for case let row as NSStackView in rows.arrangedSubviews {
+                // loop over each row and add a new web view to it
+                row.addArrangedSubview(makeWebView())
+            }
+        } else {
+            // we need to delete a column
+            // pull out the first of our rows
+            guard let firstRow = rows.arrangedSubviews.first as? NSStackView else { return }
+            
+            // make sure it has at least two columns
+            guard firstRow.arrangedSubviews.count > 1  else { return }
+            
+            // if we are still here it means it's safe to delete a column
+            for case let row as NSStackView in rows.arrangedSubviews {
+                // loop over every row
+                if let last = row.arrangedSubviews.last {
+                    // pull out the last web view in this column and remove it using the two-step process
+                    row.removeArrangedSubview(last)
+                    last.removeFromSuperview()
+                }
+            }
+        }
         
     }
     
@@ -64,9 +122,57 @@ class ViewController: NSViewController, WKNavigationDelegate {
         let webView = WKWebView()
         webView.navigationDelegate = self
         webView.wantsLayer = true
-        webView.load(URLRequest(url: URL(string:"http://www.jasonecox.org")!))
+        webView.load(URLRequest(url: URL(string:"https://www.apple.com")!))
+        
+        let recognizer = NSClickGestureRecognizer(target: self, action: #selector(webViewClicked))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+        
+        if selectedWebView == nil {
+            select(webView: webView)
+        }
         
         return webView
     }
+    
+    func select(webView: WKWebView) {
+        selectedWebView = webView
+        selectedWebView.layer?.borderWidth = 4
+        selectedWebView.layer?.borderColor = NSColor.blue.cgColor
+        
+        if let windowController = view.window?.windowController as? WindowController {
+            windowController.addressEntry.stringValue = selectedWebView.url?.absoluteString ?? ""
+        }
+    }
+    
+    @objc func webViewClicked(recognizer: NSClickGestureRecognizer) {
+        guard let newSelectedWebView = recognizer.view as? WKWebView else { return }
+        
+        // deselect the currently selected web view if there is one
+        if let selected = selectedWebView {
+            selected.layer?.borderWidth = 0
+        }
+        
+        // select the new one
+        select(webView: newSelectedWebView)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: NSGestureRecognizer, shouldAttemptToRecognizeWith event: NSEvent) -> Bool {
+        if gestureRecognizer.view == selectedWebView {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        guard webView == selectedWebView else { return }
+        
+        if let windowController = view.window?.windowController as? WindowController {
+            windowController.addressEntry.stringValue = webView.url?.absoluteString ?? ""
+        }
+    }
+    
+    
 }
 
